922ca6bef4055e5477d6e969a99708d9804968f6
Clewerton Souza <clewertonsouza8@gmail.com>
2026-09-21 22:16:58 -0400

    Push to my Site Repository

---
+++ meuPortifolio/script.js
@@ -0,0 +1,14 @@
+function ativaLetra(elemento) {
+    const arrayTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrayTexto.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    });
+}
+
+const titulo = document.querySelector('.digitando');
+if (titulo) {
+    ativaLetra(titulo);
+}
